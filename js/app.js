6e808eb7d378c4110bdeb106ba4d121bb31d7529
Serhii Myshko <sergeiown@gmail.com>
2023-01-23 18:45:50 +0200

    add news-section slider

--- js/app.js
+++ js/app.js
@@ -1,2 +1,34 @@
 /* gallery */
 baguetteBox.run(".tz-gallery");
+
+/* news slider */
+$(".news__slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  easing: "ease",
+  speed: 600,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnFocus: true,
+  pauseOnHover: true,
+  pauseOnDotsHover: true,
+  responsive: [
+    {
+      breakpoint: 950,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
